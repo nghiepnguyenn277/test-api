a7e4cd041a39e309099ed225fd2c4f636058753d
nghiepnguyenn277 <nghiepnguyenn277@gmail.com>
2022-09-15 15:24:44 +0700

    search filter 15/09/

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,11 +4,15 @@ import { Link, useNavigate } from "react-router-dom"
 import { toast } from 'react-toastify';
 
 
+
 function Home() {
     const navigate = useNavigate()
     const [data, setData] = useState([]);
     const [token, setToken] = useState(null);
     const [cate, setCate] = useState([])
+    const [search, SetSearch] = useState('')
+
+
 
     // Products
     useEffect(() => {
@@ -51,6 +55,17 @@ function Home() {
     }
     console.log("data=>>>", data)
 
+    // const handleFilter = async (e) => {
+    //     return await axios
+    //         .get(`https://nv.teknix.vn/wp-json/wc/v3/products/categories?consumer_key=ck_13bbb0868c12ba80e45d8e0649023952ee41fbf5&consumer_secret=cs_8c31c9069be46b2551b64d8c6bc11dd73239141a}`, {
+    //             headers: { Authorization: `Bearer ${token}` },
+    //         })
+    //         .then((cate) => {
+    //             setData(cate.data)
+    //         })
+    //         .catch((err) => console.log(err));
+    // }
+
 
     const onDeleteUser = async (id) => {
         if (window.confirm("are you sure delete")) {
@@ -61,7 +76,7 @@ function Home() {
             });
             if (products.status === 200) {
                 toast.success(products.data);
-                return navigate(`/`)
+
 
             }
         }
@@ -70,7 +85,27 @@ function Home() {
     }
     return (
 
+
         <div style={{ marinTop: " 150px" }}>
+
+            <h4 style={{ marginLeft: " 500px", }} >
+                Fitter by Category:
+                <select style={{ width: "auto", }}
+                >
+                    {cate.map((item) => {
+                        return (
+                            <option onChange={(e) => setCate(e.target.value)} > {item.name} </option> // lay duoc categories API , ko filter duoc product theo categories
+                        )
+                    })}
+
+                </select>
+
+            </h4>
+            <input
+                onChange={(e) => SetSearch(e.target.value)}   //search
+                placeholder='Search Name'  >
+
+            </input>
             <table className="styled-table">
                 <thead>
                     <tr>
@@ -82,14 +117,16 @@ function Home() {
                     </tr>
                 </thead>
                 <tbody>
-                    {data && data.map((item, index) => {
+                    {data.filter((item) => {
+                        return search.toLocaleLowerCase() === '' ? item : item.name.toLocaleLowerCase().includes(search);
+                        //return cate.toLocaleHightCase() === '' ? item : item.categories.toLocaleHightCase().includes(cate)
+                    }).map((item, index) => {
                         return (
                             <tr key={index}>
                                 <td>{item.id}</td>
                                 <td>{item.name}</td>
                                 <td>{item.price}</td>
                                 <td>{item.categories[0]['name']}</td>
-                                {/* <td>{item.description}</td> */}
                                 <td>
                                     <img src={item.images[0]['name'].url} alt={item.id} />
                                 </td>
@@ -103,12 +140,8 @@ function Home() {
                                             short_description: `${item.short_description}`
                                         }
                                     })}> Edit </button>
-
                                     <button style={{ color: "Green" }} onClick={() => navigate(`/add`)}> Add </button>
-
                                     <button style={{ color: "Red" }} onClick={() => onDeleteUser(item.id)}> Delete </button>
-
-
                                 </td>
                             </tr>
                         )
